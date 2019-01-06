566f1795fbdd52724dd5bb373dde9e0b2f0293bb
septsept98 <Septianardi053@gmail.com>
2019-01-06 07:47:01 +0700

    Jajal Web

--- slide.php
+++ slide.php
@@ -8,21 +8,21 @@
         </ol>
         <div class="carousel-inner" role="listbox">
           <!-- Slide One - Set the background image for this slide in the line below -->
-          <div class="carousel-item active" style="background-image: url('images/slide1.jpg');">
+          <div class="carousel-item active" style="background-image: url('images/Slide1.jpg');">
             <div class="carousel-caption d-none d-md-block">
               <h3>First Slide</h3>
               <p>This is a description for the first slide.</p>
             </div>
           </div>
           <!-- Slide Two - Set the background image for this slide in the line below -->
-          <div class="carousel-item" style="background-image: url('images/slide2.jpg')">
+          <div class="carousel-item" style="background-image: url('images/Slide2.jpg')">
             <div class="carousel-caption d-none d-md-block">
               <h3>Second Slide</h3>
               <p>This is a description for the second slide.</p>
             </div>
           </div>
           <!-- Slide Three - Set the background image for this slide in the line below -->
-          <div class="carousel-item" style="background-image: url('images/slide3.jpg')">
+          <div class="carousel-item" style="background-image: url('images/Slide3.jpg')">
             <div class="carousel-caption d-none d-md-block">
               <h3>Third Slide</h3>
               <p>This is a description for the third slide.</p>
