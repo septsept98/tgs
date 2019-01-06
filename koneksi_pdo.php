22b5ecccb865fb20ed80867d9299726418e471c1
septsept98 <Septianardi053@gmail.com>
2019-01-06 07:39:04 +0700

    Jajal Web

--- koneksi_pdo.php
+++ koneksi_pdo.php
@@ -1,7 +1,7 @@
 <?php 
 session_start();
 try{
-$conn = new PDO('mysql:host=localhost; dbname=dblab_rpl','root', ''); 
+$conn = new PDO('mysql:host=sql12.freemysqlhosting.net; dbname=sql12272865','sql12272865', 'ngDULIdhpx'); 
 $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 }
 catch(PDOException $e){
